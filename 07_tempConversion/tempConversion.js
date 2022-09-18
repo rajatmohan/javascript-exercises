79758cbdcc5594cb7138b95a2b4d369387fc7550
rajatmohan <rajatm2196@gmail.com>
2022-09-18 18:17:29 +0530

    Do exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(tempFahrenheit) {
+  return Math.round((tempFahrenheit-32)*5/9*10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(tempCelcius) {
+  return Math.round((tempCelcius*9/5+32)*10)/10;
 };
 
 // Do not edit below this line
