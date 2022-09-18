49092663391f39e347762f5b4b6152b669496746
rajatmohan <rajatm2196@gmail.com>
2022-09-18 17:32:42 +0530

    Do exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    if(times < 0) {
+        return 'ERROR'
+    }
+    let repeatedString = "";
+    while(times > 0) {
+        repeatedString += str;
+        times--;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
