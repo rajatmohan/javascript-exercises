019ffdfa8a5bb3d5c0fd4990e5099bbbfeda1dd0
rajatmohan <rajatm2196@gmail.com>
2022-09-18 17:57:00 +0530

    Do exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(param1, param2) {
+    if(!Number.isInteger(param1) || !Number.isInteger(param2)) {
+        return "ERROR";
+    }
+    if(param1 < 0 || param2 < 0) {
+        return "ERROR";
+    }
 
+    let min = Math.min(param1, param2);
+    let max = Math.max(param1, param2);
+
+    let sum = 0;
+    for(let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
