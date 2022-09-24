2d3eabd67e147872463a64fc6979926a97ff231e
rajatmohan <rajatm2196@gmail.com>
2022-09-25 00:48:23 +0530

    Do exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        let personDeathYear = (person.yearOfDeath)? person.yearOfDeath: new Date().getFullYear();
+        let oldestDeathYear = (oldest.yearOfDeath)? oldest.yearOfDeath: new Date().getFullYear();
+        if((oldestDeathYear-oldest.yearOfBirth) < (personDeathYear-person.yearOfBirth)) {
+            oldest = person;
+        }
+        return oldest;
+    });
 };
 
 // Do not edit below this line
