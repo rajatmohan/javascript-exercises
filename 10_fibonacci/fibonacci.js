b495853f80a9a14b498a7d0015e9e35223c62a25
rajatmohan <rajatm2196@gmail.com>
2022-09-25 00:29:20 +0530

    Do exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = Number(n)
+    if(n < 0 || isNaN(n)) {
+        return "OOPS";
+    }
+    if(n === 1) {
+        return 1;
+    }
+    else if(n === 2) {
+        return 1;
+    }
+    c = 2;
+    for(let i = 3, p = 1, pp = 1; i <= n; i++) {
+        c = p+pp;
+        pp = p;
+        p = c;
+    }
+    return c;
 };
 
 // Do not edit below this line
