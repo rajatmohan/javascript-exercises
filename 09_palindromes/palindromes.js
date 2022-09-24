58c1c3b2c1301faa9893acb9d5fec431cbba87db
rajatmohan <rajatm2196@gmail.com>
2022-09-25 00:21:19 +0530

    Do exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase().replace(/[^a-z0-9]/gi, '');
+    let l = 0, r = string.length-1;
+    while(l <= r) {
+        if(string[l] !== string[r]) {
+            return false;
+        }
+        l++;
+        r--;
+    }
+    return true;
 };
 
 // Do not edit below this line
