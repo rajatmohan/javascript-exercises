4417b08d8dc520f11722060f4334d64fd8d1862a
rajatmohan <rajatm2196@gmail.com>
2022-09-25 00:11:30 +0530

    Do exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(list) {
+	return list.reduce((totalSum, item) => totalSum+item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(list) {
+  return list.reduce((totalMul, item) => totalMul*item, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base**exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let fact = 1;
+  for(let i = 1; i <= n; i++) {
+    fact = fact*i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
